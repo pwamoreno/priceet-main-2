6a185c0983caa7c3ce6eaf18fe3c07dc68fe6d55
amaechichisom <amaechilegend@gmail.com>
2025-02-14 15:50:59 +0200

    Revert "Initial commit"
    This reverts commit e28026b988d499e59fb54a40824ed437c9e9f3d8.

--- src/components/set-up/index.ts
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export { default as setupAxios } from "../set-up/axios";
-// export * from '@/Setup/redux/root-reducer';
-export * from "../set-up/redux/root-reducer";
--- src/components/set-up/redux/root-reducer.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-import { combineReducers } from "redux";
-import authReducer from "../../Redux/Auth";
-
-export const rootReducer = combineReducers({
-	auth: authReducer,
-});
-export type RootState = ReturnType<typeof rootReducer>;
